a99de8d5dc7b67213d3c73645c02ba3b3035b00f
mostafa <mostafa36ahmed@gmail.com>
2021-12-07 20:45:23 +0200

    Add Project

---
+++ TaskSearching/Models/Search.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskSearching.Models
+{
+    public class Search
+    {
+        public string search_text { get; set; }
+    }
+}
